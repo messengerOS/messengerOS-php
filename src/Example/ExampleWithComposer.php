aa8d7daeace7d3b0ffab14923022b04646ba59ec
Teodor Mincu <teodor@messengeros.comm>
2023-11-17 15:18:42 +0000

    Code refactoring

--- src/Example/ExampleWithComposer.php
+++ src/Example/ExampleWithComposer.php
@@ -3,48 +3,92 @@
 // Uncomment the next line if you're using a dependency loader (such as Composer) (recommended)
 // require 'vendor/autoload.php';
 
-use MessengerOS\MessengerOS\Model\Request;
+// Uncomment the next line if you're not using a dependency loader (such as Composer),
+// replacing <PATH TO> with the path to the messengeros-loader.php file
+// require_once __DIR__ . '/../../messengeros-loader.php';
+
 use MessengerOS\MessengerOS\Model\Email;
-use MessengerOS\MessengerOS\Model\Notification;
+use MessengerOS\MessengerOS\Model\Sms;
 use MessengerOS\MessengerOS\Service\ApiService;
 
-$messengerOsDeliveryProviderKey = getenv('MESSENGER_OS_DELIVERY_PROVIDER_KEY');
+$messengerOsEmailDeliveryProviderKey = getenv('MESSENGER_OS_EMAIL_DELIVERY_PROVIDER_KEY');
+$messengerOsSmsDeliveryProviderKey = getenv('MESSENGER_OS_SMS_DELIVERY_PROVIDER_KEY');
 $messengerOsUserKey = getenv('MESSENGER_OS_USER_KEY');
 $messengerOsProjectKey = getenv('MESSENGER_OS_PROJECT_KEY');
-$messengerOsApiUrl = getenv('MESSENGER_OS_API_URL');
 
-$messengerOSRequest = new Request();
+$apiService = new ApiService(
+    $messengerOsUserKey,
+    $messengerOsProjectKey
+);
 
-$recipients[] = (new Email\EmailRecipient())->setEmail("recipient1@example.com");
-$recipients[] = (new Email\EmailRecipient())->setEmail("recipient2@example.com");
+/* Build email recipients list */
+$emailRecipients[] = (new Email\EmailRecipient())
+    ->setEmail("recipient1@example.com");
+$emailRecipients[] = (new Email\EmailRecipient())
+    ->setEmail("recipient2@example.com");
+
+/* Build SMS recipients list */
+$smsRecipients[] = (new Sms\SmsRecipient())
+    ->setPhoneNumber("07xxxxxxx1");
+$smsRecipients[] = (new Sms\SmsRecipient())
+    ->setPhoneNumber("07xxxxxxx2");
+
+// if we have to send 2 emails with different details we just build the Email objects for each of them
+// and add them to the $emails array
 
 $email = new Email\Email();
 $email
     ->setFromName("John | My Company")
     ->setFromEmail("john@mycompany.com")
-    ->setRecipients($recipients)
+    ->setRecipients($emailRecipients)
     ->setSubject("My subject line from API")
     ->setPreviewLine("My preview line from API")
-    ->setDeliveryProvider($messengerOsDeliveryProviderKey)
+    ->setDeliveryProvider($messengerOsEmailDeliveryProviderKey)
+    ->setTemplate("_OPTIONAL_TEMPLATE_API_KEY_FROM_PLATFORM")
     ->setReplyTo("no-reply@mycompany.com")
     ->setBody("<p>My HTML Content</p>")
-    ->setParams([['first_name' => 'Maurice']]);
+    ->setParams([
+        ['first_name' => 'Thomas'],
+        ['last_name' => 'Smith']
+    ]);
 
 $emails[] = $email;
 
-$notification = (new Notification())
-    ->setEmail($emails);
+// if we have to send 2 SMSes with different details we just build the Sms objects for each of them
+// and add them to the $SMSes array
+$sms = new Sms\Sms();
+$sms
+    ->setRecipients($smsRecipients)
+    ->setDeliveryProvider($messengerOsSmsDeliveryProviderKey)
+    ->setSmsBody("My text content")
+    ->setTemplate("_OPTIONAL_TEMPLATE_API_KEY_FROM_PLATFORM")
+    ->setParams([
+        ['first_name' => 'Thomas'],
+        ['last_name' => 'Smith']
+    ]);
 
-$messengerOSRequest
-    ->setUser($messengerOsUserKey)
-    ->setProject($messengerOsProjectKey)
-    ->setNotification($notification);
+$SMSes[] = $sms;
 
-$apiService = new ApiService();
-
+// Send mixed notifications with Emails and Smses at once //
 try {
-    $response = $apiService->send($messengerOsApiUrl, json_encode($messengerOSRequest));
+    $response = $apiService->sendNotifications($emails, $SMSes);
+    print $response . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
 
+// Send only Email notifications //
+try {
+    $response = $apiService->sendEmails($emails);
+    print $response . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
+
+
+// Send only SMS notifications //
+try {
+    $response = $apiService->sendSMSes($SMSes);
     print $response . "\n";
 } catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
--- src/Service/ApiService.php
+++ src/Service/ApiService.php
@@ -2,15 +2,61 @@
 
 namespace MessengerOS\MessengerOS\Service;
 
+use MessengerOS\MessengerOS\Model\Notification;
+use MessengerOS\MessengerOS\Model\Request;
+
 class ApiService
 {
-    public function send($url, $data)
+    private string $messengerOsUserKey;
+    private string $messengerOsProjectKey;
+
+    public function __construct(
+        $messengerOsUserKey,
+        $messengerOsProjectKey
+    ) {
+        $this->messengerOsUserKey = $messengerOsUserKey;
+        $this->messengerOsProjectKey = $messengerOsProjectKey;
+    }
+
+    public function sendNotifications(array $emails, array $SMSes)
     {
+        $notification = (new Notification())
+            ->setEmail($emails)
+            ->setSms($SMSes);
+        return $this->sendNotification($notification);
+    }
+
+    public function sendSMSes(array $SMSes)
+    {
+        $notification = (new Notification())
+            ->setSms($SMSes);
+        return $this->sendNotification($notification);
+    }
+
+
+    public function sendEmails(array $emails)
+    {
+        $notification = (new Notification())
+            ->setEmail($emails);
+        return $this->sendNotification($notification);
+    }
+
+    public function sendNotification($notification)
+    {
+        $messengerOSRequest = new Request();
+        $messengerOSRequest
+            ->setUser($this->messengerOsUserKey)
+            ->setProject($this->messengerOsProjectKey)
+            ->setNotification($notification);
+
+        $url = "https://inbound.messengeros.com/1.0/send";
+
+        var_dump(json_encode($messengerOSRequest));
         $ch = curl_init();
         curl_setopt_array($ch, array(
             CURLOPT_URL => $url,
             CURLOPT_POST => true,
-            CURLOPT_POSTFIELDS => $data,
+            CURLOPT_POSTFIELDS => json_encode($messengerOSRequest),
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_HEADER => false,
             CURLOPT_SSL_VERIFYPEER => false
